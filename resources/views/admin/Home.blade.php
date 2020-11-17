702c2b80c2142c5ebb75398ef70bb93ac2b1b815
dinel <solahuddin.hafid.tomaballo@gmail.com>
2020-11-18 00:49:09 +0800

    perbaiki tampilan dasbord

--- resources/views/admin/Home.blade.php
+++ resources/views/admin/Home.blade.php
@@ -1,5 +1,18 @@
 @extends('admin.template.default')
 
 @section('content')
-    <h1>Dashboard</h1>
+<h1 class="text-bold">Dashboard</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <img src="{{ asset('images/rs.png') }}" alt="rumah sakit" width="340">
+        </div>
+        <div class="col-6">
+            <p>Alamat : <span class="text-bold"> Jl Nangka, Bontomarannu, Gowa</span></p>
+            <p>Fasilitas : <span class="text-bold">Poligigi, Polikandungan, UGD, ICU</span></p>
+            <p>Email :<span class="text-bold"> permata.hati@gmail.com </span></p>
+            <p>No Hp :<span class="text-bold"> 08222334455 </span></p>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/admin/template/partial/sidebar.blade.php
+++ resources/views/admin/template/partial/sidebar.blade.php
@@ -1,54 +1,72 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="#" class="brand-link">
-      <span class="brand-text font-weight-light">RS Permata Hati</span>
-    </a>
+<aside class="main-sidebar bg-success elevation-4">
+  <!-- Brand Logo -->
+  <a href="#" class="brand-link">
+    <span class="brand-text font-weight-light font-weight-bold">RS Permata Hati</span>
+  </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="info">
-          <a href="#" class="d-block">nama anda</a>
-        </div>
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="info">
+        <a href="#" class="d-block text-light">Admin Rumah sakit</a>
       </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-         
-          
-          <li class="nav-header">Main Navigation</li>
-          <li class="nav-item">
-            <a href="{{route('uang.index') }}" class="nav-link">
-              <i class="fas fa-book"></i>
-              <p>Keuangan</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="{{route('pasien.index') }}" class="nav-link">
-              <i class="fas fa-users"></i>
-              <p>Data Pasien</p>
-            </a>
-          </li>
-           <li class="nav-item">
-            <a href="{{route('pegawai.index') }}" class="nav-link">
-              <i class="fas fa-user"></i>
-              <p>Pegawai</p>
-            </a>
-          </li>     
-          <li class="nav-item">
-            <a href="{{route('gudang.index') }}" class="nav-link">
-              <i class="fas fa-book"></i>
-              <p>Gudang</p>
-            </a>
-          </li>     
-         
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
     </div>
-    <!-- /.sidebar -->
-  </aside>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+
+        <li class="nav-header">Main Navigation</li>
+        <li class="nav-item">
+          <a href="{{route('uang.index') }}" class="nav-link">
+            <i class="fas fa-book text-light"></i>
+            <p class="text-light">Tagihan</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="{{route('pasien.index') }}" class="nav-link">
+            <i class="fas fa-users text-light"></i>
+            <p class="text-light">Data Pasien</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="{{route('pegawai.index') }}" class="nav-link">
+            <i class="fas fa-user text-light"></i>
+            <p class="text-light">Pegawai</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="{{route('gudang.index') }}" class="nav-link">
+            <span class="text-light">
+              <i class="fas fa-book "></i>
+              <p class="text-light">laboratory</p>
+            </span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="{{route('gudang.index') }}" class="nav-link">
+            <span class="text-light">
+              <i class="fas fa-hospital "></i>
+              <p class="text-light">Kamar</p>
+            </span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="{{route('gudang.index') }}" class="nav-link">
+            <span class="text-light">
+              <i class="fas fa-user-md "></i>
+              <p class="text-light">Dokter</p>
+            </span>
+          </a>
+        </li>
+
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
